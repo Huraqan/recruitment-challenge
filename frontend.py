cd8913650b03df25b01fd271dee98286622a1c22
unknown <indesteege.sebastiaan@gmail.com>
2024-04-12 16:10:37 +0200

    1st upload

---
+++ frontend.py
@@ -0,0 +1,38 @@
+import requests
+import streamlit as st
+import json
+
+with open("values.json", "r") as f:
+    values = json.load(f)
+
+Shop = st.selectbox("Shop", values["Shop"])
+BrandName = st.selectbox("BrandName", values["BrandName"])
+ModelGroup = st.selectbox("ModelGroup", values["ModelGroup"])
+ProductGroup = st.selectbox("ProductGroup", values["ProductGroup"])
+OriginalSaleAmountInclVAT = st.number_input("OriginalSaleAmountInclVAT")
+Day = st.selectbox("Day", values["day"])
+Month = st.selectbox("Month", values["month"])
+
+
+# fetch api when button is clicked
+if st.button("Predict!"):
+    inputs = {
+        "Shop": int(Shop),
+        "BrandName": int(BrandName),
+        "ModelGroup": int(ModelGroup),
+        "ProductGroup": int(ProductGroup),
+        "day": Day,
+        "month": Month,
+        "OriginalSaleAmountInclVAT": float(OriginalSaleAmountInclVAT),
+    }
+    
+    try:
+        r = requests.post(url="http://127.0.0.1:8000/pred", json=inputs)
+        if r.status_code == 200:
+            response_data = r.json()
+            prediction = response_data["pred"]
+            st.subheader(f"Return?: {prediction}")
+        else:
+            st.error(f"Error: {r.text}")
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
---
+++ predict.py
@@ -0,0 +1,36 @@
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.model_selection import train_test_split#, GridSearchCV
+# from sklearn.metrics import classification_report, roc_curve, auc, roc_auc_score
+# import sklearn
+import joblib
+import pandas as pd
+
+
+def predict(input):
+    row_dict = {k:[v] for k, v in dict(input).items()}
+    data = pd.DataFrame(row_dict)
+    
+    # Load and unpack the model
+    artifacts = joblib.load("artifacts_rf_better.joblib")
+    
+    num_features = artifacts["features"]["num_features"]
+    cat_features = artifacts["features"]["cat_features"]
+    enc = artifacts["enc"]
+    model = artifacts["model"]
+    
+    data = data[num_features + cat_features]
+    
+    # Apply imputer, scaler and encoder on data
+    data_cat = enc.transform(data[cat_features])
+    
+    # Combine the numerical and one-hot encoded categorical columns
+    data = pd.concat(
+        [
+            pd.DataFrame(data_cat, columns=enc.get_feature_names_out()),
+            data[num_features].reset_index(drop=True),
+        ],
+        axis=1,
+    )
+    
+    prediction = {"pred": model.predict(data)[0]}
+    return prediction
